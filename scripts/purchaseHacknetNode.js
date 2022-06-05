e8dff18339411eb962b80a6be85d0a797793a789
robertfrank84 <robertfrank.rf.rf@gmail.com>
2022-06-05 14:58:47 +0200

    Fixing and improving purchaseHacknetNode.js
    fixed loop condition
    added flags for max number of nodes and waiting time
    added extended Log functionality

--- scripts/purchaseHacknetNode.js
+++ scripts/purchaseHacknetNode.js
@@ -1,51 +1,66 @@
-/** @param {NS} ns **/
+import {ExtendedLog} from "/scripts/lib/extendedLog";
+
+/** @member {ExtendedLog} */
+let el = null;
+
+const SCRIPT_NAME = 'purchaseHacknetNode.js';
+
+/** @param {NS} ns */
 export async function main(ns) {
+    // TODO: Helper that shows all possible flags in the terminal
+    const flags = ns.flags([
+        ['n', 25],  // max number of nodes
+        ['w', 10000] // time to wait in ms
+    ]);
+
+    el = new ExtendedLog(ns, SCRIPT_NAME);
+
+    /** @returns {number} */
     function myMoney() {
         return ns.getServerMoneyAvailable("home");
     }
 
-    ns.disableLog("getServerMoneyAvailable");
-    ns.disableLog("sleep");
+    el.start();
 
-    while (true) {
+    while (ns.hacknet.numNodes() <= flags.n) {
 
         // TODO: Check if all nodes are maxed out
 
         if (myMoney() > ns.hacknet.getPurchaseNodeCost()) {
             let nodeIndex = ns.hacknet.purchaseNode();
-            ns.tprint("Purchase hacknet Node #" + nodeIndex);
+            el.log('Purchase hacknet node #' + nodeIndex);
 
-            while (ns.hacknet.getLevelUpgradeCost(nodeIndex, 1) !== "Infinity") {
+            while (ns.hacknet.getLevelUpgradeCost(nodeIndex, 1) !== Infinity) {
                 if (myMoney() > ns.hacknet.getLevelUpgradeCost(nodeIndex, 1)) {
                     ns.hacknet.upgradeLevel(nodeIndex, 1);
                 } else {
-                    await ns.sleep(10000);
+                    await ns.sleep(flags.w);
                 }
             }
 
-            ns.print("Hacknet node #" + nodeIndex + " at max level!");
+            el.log('Hacknet node #' + nodeIndex + ' at max level!');
 
-            while (ns.hacknet.getRamUpgradeCost(nodeIndex, 1) !== "Infinity") {
+            while (ns.hacknet.getRamUpgradeCost(nodeIndex, 1) !== Infinity) {
                 if (myMoney() > ns.hacknet.getRamUpgradeCost(nodeIndex, 1)) {
                     ns.hacknet.upgradeRam(nodeIndex, 1);
                 } else {
-                    await ns.sleep(10000);
+                    await ns.sleep(flags.w);
                 }
             }
 
-            ns.print("Hacknet node #" + nodeIndex + " at max RAM!");
+            el.log('Hacknet node #' + nodeIndex + ' at max RAM!');
 
-            while (ns.hacknet.getCoreUpgradeCost(nodeIndex, 1) !== "Infinity") {
+            while (ns.hacknet.getCoreUpgradeCost(nodeIndex, 1) !== Infinity) {
                 if (myMoney() > ns.hacknet.getCoreUpgradeCost(nodeIndex, 1)) {
                     ns.hacknet.upgradeCore(nodeIndex, 1);
                 } else {
-                    await ns.sleep(10000);
+                    await ns.sleep(flags.w);
                 }
             }
 
-            ns.print("Hacknet node #" + nodeIndex + " at max core!");
+            el.log('Hacknet node #' + nodeIndex + ' at max core!');
         }
 
-        await ns.sleep(30000);
+        await ns.sleep(flags.w);
     }
 }
