4175d681ed2761a508143cb03f6fc698b35a4101
robertfrnak84 <robertfrank.rf.rf@gmail.com>
2022-07-06 23:41:20 +0200

    Improving EZspread-v2.js to v2.3
    target server is now checked for running scripts
    running scripts are killed
    hackScript is updated on target server
    loop condition is now working

--- scripts/EZspread-v2.js
+++ scripts/EZspread-v2.js
@@ -11,7 +11,7 @@ let n = null;
 let xs = null;
 
 const SCRIPT_NAME = 'EZspread.js';
-const VERSION = '2.2';
+const VERSION = '2.3';
 
 /** @param {NS} ns **/
 export async function main(ns) {
@@ -23,29 +23,43 @@ export async function main(ns) {
 
     const hackScript = '/scripts/EZhack.js';
     const allScripts = ns.ls("home", "/scripts/");
-    const targets = n.getAllServerData().filter(server => server.maxRam > 0 && !server.purchasedByPlayer);
-    const numOfTargets = targets.length;
-    let doneTargets = 0;
+    let targets;
+    let numOfTargets;
+    let doneTargets;
 
-    while (doneTargets <= numOfTargets) {
-        xs.getServerListAccess(targets); // works -- all targets nuked
-        for ( let target of targets) {
-            const serverName = target.hostname
-            const maxThreads = Math.floor(target.maxRam / ns.getScriptRam(hackScript));
+    do {
+        targets = n.getAllServerData().filter(server => server.maxRam > 0 && !server.purchasedByPlayer);
+        numOfTargets = targets.length;
+        doneTargets = 0;
+        xs.getServerListAccess(targets); // nuke targets
+        for (let target of targets) {
+            if (target.numOpenPortsRequired <= xs.getAvailablePrograms().length) {
+                const serverName = target.hostname
+                const maxThreads = Math.floor(target.maxRam / ns.getScriptRam(hackScript));
 
-            // TODO: remove old script from target
+                // checks if target has script running
+                if (target.ramUsed > 0) {
+                    ns.killall(serverName);
+                    ns.rm(hackScript, serverName);
+                }
 
-            await ns.scp(allScripts, serverName);
-            let runningScript = ns.exec(hackScript, serverName, maxThreads);
+                await ns.scp(allScripts, serverName);
+                let runningScript = ns.exec(hackScript, serverName, maxThreads);
 
-            if (runningScript !== 0) {
-                ++doneTargets;
+                if (runningScript !== 0) {
+                    ++doneTargets;
+                    el.log( serverName + " hacked, " + doneTargets + "/" + numOfTargets);
+                } else {
+                    el.log( serverName + " wasn't hacked");
+                }
             } else {
-                el.log( serverName + " wasn't hacked");
+                el.log('still programs missing ...');
             }
         }
-        el.separate('separate')
-        await ns.sleep(180000);
-    }
+        if (doneTargets < numOfTargets) {
+            el.separate('separate')
+            await ns.sleep(10000);
+        }
+    } while (doneTargets < numOfTargets);
     el.end();
 }
--- scripts/tmp.js
+++ scripts/tmp.js
@@ -1,11 +1,15 @@
 import {ExtendedLog} from "/scripts/lib/extendedLog";
 import {Network} from "/scripts/lib/network";
+import {GetAccess} from "/scripts/lib/getAccess";
 
 /** @member {ExtendedLog} */
 let el = null;
 
 /** @member {Network} */
 let n = null;
+
+/** @member {GetAccess} */
+let xs = null;
 
 const SCRIPT_NAME = 'tmp.js';
 const FILE_VERSION = '1';
@@ -14,14 +18,16 @@ const FILE_VERSION = '1';
 export async function main(ns) {
     el = new ExtendedLog(ns, SCRIPT_NAME, FILE_VERSION);
     n = new Network(ns);
+    xs = new GetAccess(ns);
 
     el.start();
 
     // el.log('hostnames: ', n.getKnownServers());
+    el.log(xs.getAvailablePrograms().length)
 
     el.log(n.getServerByHostname('the-hub'));
     // el.log(n.getServerByHostname('home'));
-    el.logRaw(ns.getPlayer());
+    // el.logRaw(ns.getPlayer());
     // el.logRaw(btoa(Date.now().toString()));
     // el.logRaw(Date.now().toString());
     el.end();
