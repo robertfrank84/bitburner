2dce918521d44f4b16ab4428d50a1724d2ea1c70
robertfrnak84 <robertfrank.rf.rf@gmail.com>
2022-07-07 23:03:57 +0200

    Adding a Loop counter and fixing the while condition of EZhack.js

--- scripts/EZhack.js
+++ scripts/EZhack.js
@@ -4,7 +4,7 @@ import {ExtendedLog} from "/scripts/lib/extendedLog";
 let el = null;
 
 const SCRIPT_NAME = 'EZhack.js';
-const VERSION = '1.1';
+const VERSION = '1.2';
 
 /** @param {NS} ns */
 export async function main(ns) {
@@ -14,11 +14,12 @@ export async function main(ns) {
     const target = ns.args[0] || 'the-hub';
     const moneyThresh = ns.getServerMaxMoney(target) * 0.75;
     const securityThresh = ns.getServerMinSecurityLevel(target) + 2;
+    let loop = 0
 
     el.start();
 
     // Infinite loop that continously hacks/grows/weakens the target server
-    while (true) {
+    while (loop >= 0) {
         const serverMoney = Math.round(ns.getServerMoneyAvailable(target) * 100) / 100;
         const percent = Math.round(((serverMoney / moneyThresh) * 100) * 100) / 100;
 
@@ -26,13 +27,16 @@ export async function main(ns) {
             let weakenBy = await ns.weaken(target);
             el.log('Security level lowered by: ' + weakenBy);
         } else if (serverMoney < moneyThresh) {
-            el.log(percent + "% - " + ns.nFormat(serverMoney, '($0.00a)') + "/" + ns.nFormat(moneyThresh, '($0.00a)') + " (Money/max Money)");
             await ns.grow(target);
+            el.log(percent + "% - " + ns.nFormat(serverMoney, '($0.00a)') + "/" + ns.nFormat(moneyThresh, '($0.00a)') + " (Money/max Money)");
         } else {
             // Otherwise, hack it
             let loot = await ns.hack(target);
             el.log('Money available: ' + ns.nFormat(serverMoney, '($0.00a)'));
             el.log('got ' + ns.nFormat(loot, '($0.00a)'));
         }
+
+        el.log('loop ' + ++loop);
+        await ns.sleep(10);
     }
 }
